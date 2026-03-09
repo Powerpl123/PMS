5b289af5a6ffe9bee424009cb1f5b1598f7e8ded
Gisagara <it@hqpower-rwanda.com>
2026-03-09 15:56:26 +0000

    feat: production build setup - Express serves React, build scripts

--- src/app.js
+++ src/app.js
@@ -2,19 +2,30 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import { fileURLToPath } from "url";
+import path from "path";
 import routes from "./routes/index.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
 import { notFound } from "./middlewares/notFound.js";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "2mb" }));
 
 app.use("/api", routes);
-app.use(notFound);
+
+/* Serve React build in production */
+const clientDist = path.join(__dirname, "..", "client", "dist");
+app.use(express.static(clientDist));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(clientDist, "index.html"));
+});
+
 app.use(errorHandler);
 
 export default app;
